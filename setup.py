cfa6cc579ba443a34ba6a0203acfabd5d5801904
Mike Doan <spikedoanz@gmail.com>
2024-08-08 18:13:37 -0400

    gca

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+from setuptools.dist import Distribution
+
+# Function to parse the requirements file
+def parse_requirements(filename):
+    with open(filename) as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+# Packages required for setup
+setup_requires = ['numpy']
+
+# Packages required for installation
+install_requires = parse_requirements('requirements.txt')
+
+# Attempt to import numpy, but don't fail if it's not available
+try:
+    import numpy
+    numpy_include = [numpy.get_include()]
+except ImportError:
+    numpy_include = []
+
+# Define the extension module
+nd_image_module = Extension('brainchop.utils._nd_image',
+                            sources=['brainchop/utils/nd_image/nd_image.c',
+                                     'brainchop/utils/nd_image/ni_filters.c',
+                                     'brainchop/utils/nd_image/ni_fourier.c',
+                                     'brainchop/utils/nd_image/ni_interpolation.c',
+                                     'brainchop/utils/nd_image/ni_measure.c',
+                                     'brainchop/utils/nd_image/ni_morphology.c',
+                                     'brainchop/utils/nd_image/ni_splines.c',
+                                     'brainchop/utils/nd_image/ni_support.c'],
+                            include_dirs=numpy_include,
+                            extra_compile_args=['-std=c99'])
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -13,12 +45,9 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/neuroneural/brainchop-cli",
     packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "tinygrad",
-        "nibabel",
-        "requests",
-    ],
+    ext_modules=[nd_image_module],
+    setup_requires=setup_requires,
+    install_requires=install_requires,
     entry_points={
         "console_scripts": [
             "brainchop=brainchop.main:main",
