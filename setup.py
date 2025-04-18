cc0d84a9437b8e78b513adece7ccaaa6ba048ad3
spike <spikedoanz@gmail.com>
2025-04-18 11:02:26 -0400

    new version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def copy_niimath_executable(setup_kwargs):
 
 setup(
     name="brainchop",
-    version="0.1.8",
+    version="0.1.9",
     author="Mike Doan",
     author_email="spikedoanz@gmail.com",
     description="Portable and lightweight brain segmentation using tinygrad",
