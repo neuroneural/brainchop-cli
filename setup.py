b0fdcea5fbf63097b1b449408794a278396ade4a
Sergey Plis <s.m.plis@gmail.com>
2025-05-25 14:52:06 -0400

    Bump version to 0.1.13

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = Path("requirements.txt").read_text().splitlines()
 
 setup(
     name="brainchop",
-    version="0.1.12",
+    version="0.1.13",
     author="Mike Doan",
     author_email="spikedoanz@gmail.com",
     description="Portable and lightweight brain segmentation using tinygrad",
