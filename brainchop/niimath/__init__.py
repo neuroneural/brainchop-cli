515ab84f73f8a3640b702dc7ea875204e0e584e6
Sergey Plis <s.m.plis@gmail.com>
2025-05-25 20:49:24 -0400

    further simplified largest cluster computation (hopefully faster)

--- brainchop/niimath/__init__.py
+++ brainchop/niimath/__init__.py
@@ -189,10 +189,10 @@ def conform(input_image_path, output_image_path="conformed.nii", comply=False):
     return conform_img, header
 
 
-def largest_cluster_mask(data):
+def largest_cluster(data):
     counts = np.bincount(data.ravel().astype(np.int32))
-    largest_label = int(counts[1:].argmax() + 1)
-    return data == largest_label
+    largest_label = counts[1:].argmax() + 1
+    return largest_label
 
 
 def bwlabel(image_path, neighbors=26, image=None):
@@ -217,12 +217,12 @@ def bwlabel(image_path, neighbors=26, image=None):
     _run_niimath(args)
 
     if image is None:
-        image = _read_nifti(image_path)[0]
+        image = _read_nifti(image_path)[0].astype(np.uint8)
 
     clusters, header = _read_nifti(mask_path)
-    mask = largest_cluster_mask(clusters)
-
-    _write_nifti(image_path, (mask * image).astype(np.uint8), header)
+    cluster_label = largest_cluster(clusters)
+    image[clusters != cluster_label] = 0
+    _write_nifti(image_path, image, header)
 
     try:
         mask_path.unlink()  # Use pathlib's unlink instead of subprocess rm
