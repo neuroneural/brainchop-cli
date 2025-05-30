61f31e1cace8cc37993ee7127c0778b160981495
Sergey Plis <s.m.plis@gmail.com>
2025-05-30 11:19:12 -0400

    bumping up the version for correct mindgrab reslicing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = Path("requirements.txt").read_text().splitlines()
 
 setup(
     name="brainchop",
-    version="0.1.16.post1",
+    version="0.1.17",
     author="Mike Doan",
     author_email="spikedoanz@gmail.com",
     description="Portable and lightweight brain segmentation using tinygrad",
